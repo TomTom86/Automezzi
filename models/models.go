050616f04fa3dd2a699e24b88311f5ccf7ca12a9
unknown <tmontebello@PC24.ECOSI.local>
2015-12-18 17:34:03 +0100

    Progetto 0.1 + TODO

--- models/models.go
+++ models/models.go
@@ -19,6 +19,26 @@ type AuthUser struct {
 	Group 	  int
 }
 
+
+/*
+password
+password salt
+mobile pin
+email
+password question
+is approved
+is locked out
+create data
+last login date
+last password change date
+last lock out date
+failed password attempt count
+password attempt windows start
+
+comment
+
+
+*/
 func init() {
 	orm.RegisterModel(new(AuthUser))
 }
